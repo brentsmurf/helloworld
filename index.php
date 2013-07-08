033363d91e4e8277a4987dfb7631516d7ab3a95c
brentsmurf <brentlevinga@gmail.com>
2013-07-08 23:03:39 +0200

    html

--- index.php
+++ index.php
@@ -1,17 +1,44 @@
-<?php
+<html>
+	<head>
+		<title>Brent's website</title>
+	</head>
+	<body>
+		<header>
+			<h1>Brent's website</h1>
+			
+		</header>
+		<nav>
+			<ul>
+				<li><a href='/test.php'>optie1</a></li>
+				<li>optie1</li>
+				<li>optie1</li>
+				<li>optie2</li>
 
-
-	$i = 5;
-
-	if($i > 10)
-	{
-		$foo = 'hoi';
-	}
-	else 
-	{
-		$foo = 'doei';
-	}
-
- 	echo $foo . " world";
-
-?>
+			</ul>
+		</nav>	
+		<section>
+			<article>
+				<h2>kopje</h2>  
+				<img src="http://6.media.hyves-static.net/1183457773/6/z1Qc/0/img1183457773.jpeg">
+				<p>springen BMX zoetermeer <br>
+				lengte bult <strong>6 meter</strong> en <em> 3 meter hoog</em>
+				</p>
+				<table>
+					<tr>
+						<td>Hoogte</td>
+						<td>3 meter</td>
+					</tr>		
+					<tr>
+						<td>lengte</td>
+						<td>6 meter</td>
+					</tr>
+					<tr>
+						<td>Snelheid</td>
+						<td>Sneller dan het licht</td>
+					</tr>
+				</table>
+			</article> 
+		</section>
+		
+	</body>
+</html>
---
+++ test.php
@@ -0,0 +1,20 @@
+<html>
+	<head>
+		<title>Brent's website</title>
+	</head>
+	<body>
+
+		<nav>
+			<ul>
+				<li><a href='/index.php'>optie1</a></li>
+				<li><a href='/'>optie1</a></li>
+				<li>optie1</li>
+				<li>optie1</li>
+				<li>optie2</li>
+
+			</ul>
+	
+		
+		</nav>	
+	</body>
+</html>
